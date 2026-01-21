9939e3130f2b3a453c44c6a1a1b74bd6d6fa8c57
Anirudh Shaklya <anirudhshaklya001p@gmail.com>
2026-01-21 21:53:35 +0530

    Added ai.py

---
+++ ai_logic.py
@@ -0,0 +1,21 @@
+import vertexai
+from vertexai.generative_models import GenerativeModel
+
+#fill project id while deploying
+vertexai.init(priject='177013', location="asia-south2")
+
+def analyze_market_sentiment(Headline_News):
+    model = GenerativeModel("gemini-1.5-flash-001")
+
+    prompt = f"""Analyze the following financial news headline. Determine if the news is related to the technology sector.
+            If yes: Return a 'Risk Score' between 0.0 (Safe) and 0.05 (High Risk of Crash).
+            If no: Return exactly 0.0 .
+            Only return the number
+                Headline: "{news_headline}" """
+    
+    response = model.generate_content(prompt)
+
+    try:
+        return float(response.text.strip())
+    except:
+        return 0.01 # Default fallback
