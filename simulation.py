ecef007363c2c5495904f2fea34a771bdd35ba82
Anirudh Shaklya <anirudhshaklya001p@gmail.com>
2026-01-27 23:39:37 +0530

    added a histogram plot

--- simulation.py
+++ simulation.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf 
-def fetch_data(tickers, days=365):
+def fetch_data(tickers, period="5y"):
 
     # Force 'Adj Close' to appear by setting auto_adjust=False
-    data = yf.download(tickers, period="5y", auto_adjust=False)['Adj Close']
+    data = yf.download(tickers, period=period, auto_adjust=False)['Adj Close']
+    if isinstance(data, pd.Series):
+        data = data.to_frame()
 
     # get returns
     returns = data.pct_change().dropna() # drop NA values and percentage change
@@ -48,6 +50,30 @@ def monte_carlo(mean_returns, cov_matrix, weights,investment_amount ,num_sims=10
     # 3. Extract the full path (all days) for these specific simulations
     max_path = sim_data[:, max_idx]
     min_path = sim_data[:, min_idx]
-    return min_path, max_path
+    return min_path, max_path, sim_data
+
+def calculate_kpis(sim_data, investment_amount):
+    final_values = sim_data[-1, :]
+
+    #calculating expected return
+    expected_value = np.mean(final_values)
+    expected_return_pct = (expected_value - investment_amount) / investment_amount
+
+    # VaR(95%)
+    cutoff_95 = np.percentile(final_values, 5)
+    var_95 = investment_amount - cutoff_95
+
+    # 3. Conditional Value at Risk (CVaR)
+    # The average loss of the worst 5% cases (Tail Risk)
+    worst_5_percent = final_values[final_values <= cutoff_95]
+    cvar_95 = investment_amount - worst_5_percent.mean()
+
+    # 4. Probability of Success (Breakeven)
+    # How many scenarios ended up higher than the starting amount?
+    success_count = np.sum(final_values > investment_amount)
+    prob_success = success_count / len(final_values)
+
+    return expected_value, expected_return_pct, var_95, cvar_95, prob_success
+
 
     
